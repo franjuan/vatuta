7ff971400a7d047092316cc9ff18ee95c202dbe6
franjuan <juanfran.adame@gmail.com>
2015-08-12 00:34:27 +0200

    Playing with docs and site

--- src/main/javascript/vatuta.js
+++ src/main/javascript/vatuta.js
@@ -1,3 +1,20 @@
 /**
- * 
+ * Vatuta library
  */
+
+/**
+ * Task
+ * @constructor
+ * @param {string} Description string for representation of task, no processing over it.
+ * @return A new task type
+ */
+function Task(description) {
+	this.description = description;
+}
+/**
+ * Returns task's description
+ * @returns {string}
+ */
+Task.prototype.getDescription = function() {
+	return this.description;
+}
