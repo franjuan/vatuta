7d8b8dd7c42acc062bed969602a8ba2d20584f22
franjuan <juanfran.adame@gmail.com>
2015-08-13 00:49:38 +0200

    Painting first tasks

--- src/main/javascript/vatuta.js
+++ src/main/javascript/vatuta.js
@@ -6,10 +6,12 @@
  * Task
  * @constructor
  * @param {string} Description string for representation of task, no processing over it.
+ * @param {number} Duration in days of task
  * @return A new task type
  */
-function Task(description) {
+function Task(description, duration) {
 	this.description = description;
+	this.duration = duration;
 }
 /**
  * Returns task's description
@@ -17,4 +19,31 @@ function Task(description) {
  */
 Task.prototype.getDescription = function() {
 	return this.description;
+}
+/**
+ * Returns task's duration in days
+ * @returns {string}
+ */
+Task.prototype.getDuration = function() {
+	return this.duration;
+}
+
+Task.prototype.getContainer = function() {
+	var container = new createjs.Container();
+	
+	// Add a shape
+	var shape = new createjs.Shape();
+	shape.graphics.beginFill("DeepSkyBlue");
+	shape.graphics.drawRect(0, 0, 10*this.getDuration(), 50);
+	container.addChild(shape);
+	
+	// Add a label
+	var text = new createjs.Text(this.getDescription(),"20px Arial", "White");
+	text.textAlign="center";
+	text.x=10*this.getDuration()/2;
+	text.y=50/2;
+	text.m
+	container.addChild(text);
+
+	return container;
 }
