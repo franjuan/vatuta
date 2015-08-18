6887305c719b9b581172f3494b3752859aa22ded
JuanFran <juanfran.adame@gmail.com>
2015-08-19 00:26:54 +0200

    Fichero JSON descriptor del proyecto creado

---
+++ src/main/javascript/project.js
@@ -0,0 +1,11 @@
+var project = JSON.parse('{' +
+	'"name": "Proyecto",' +
+	'"streams": [' +
+	'		 	{	"name": "Stream",' +
+	'		 		"tasks": 	[' +
+	'		 					{"description":"Tarea1", "duration":5},' +
+	'		 					{"description":"Tarea2", "duration":15}' +
+	'		 				]' +
+	'		 	}' +
+	'		 ]' +
+'}');
